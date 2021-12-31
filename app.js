e47f144053eb72cbf20fb921b843dfdccfd47bbf
Romiest <marestrio2001@gmail.com>
2021-12-31 04:07:44 +0200

    done decreasing contrast

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function displayOriginalImage(event) {
       document.getElementById("transformation").style.display = "initial";
       document.getElementById("result").style.display = "none";
     }
-  }//
+  }
 }
 
 //Makes sure the uploaded file is a png or jpg image 
@@ -262,6 +262,15 @@ function showTransformForm() {
     var valData2;
     var valData3;
     
+    var slopeBefore = (TransBrightestDark / originalBrightestDark);
+    var cbefore = 0;
+    
+    var slopeAfter = (255-TransDarkestBright) / (255 - originalDarkestBright);
+    var cAfter = TransDarkestBright - (slopeAfter * originalDarkestBright);
+
+    var slope = (TransDarkestBright - TransBrightestDark)/(originalDarkestBright  - originalBrightestDark);
+    var c = TransBrightestDark - (slope * originalBrightestDark);
+    
     if (originalBrightestDark  == originalDarkestBright){
       document.getElementById("error").style.display = "block";
       console.log("This is an error");
@@ -276,11 +285,34 @@ function showTransformForm() {
     //Images are displayed in the RGBA format so a greyscale pixel could look like (25,25,25,255)
     rgba = getRGBAValues(img, canvas, ctx);
 
+   
     for (i = 0; i < img.width * img.height * 4; i += 4) {
+      
       valData1 = rgba[i] * slope + c ;
       valData2 = rgba[i+1] * slope + c ;
       valData3 = rgba[i+2] * slope + c ;
       
+
+      if (rgba[i]  < originalBrightestDark){
+        valData1 = rgba[i] * slopeBefore + cbefore ;
+      }else if (rgba[i] > originalDarkestBright){
+        valData1 = rgba[i] * slopeAfter + cAfter ;
+      }
+      
+      if (rgba[i+1]  < originalBrightestDark){
+        valData2 = rgba[i+1] * slopeBefore + cbefore ;
+      }else if (rgba[i+1] > originalDarkestBright){
+        valData2 = rgba[i+1] * slopeAfter + cAfter ;
+      }
+      
+      if (rgba[i+2]  < originalBrightestDark){
+        valData3 = rgba[i+2] * slopeBefore + cbefore ;
+      }else if (rgba[i+2] > originalDarkestBright){
+        valData3 = rgba[i+2] * slopeAfter + cAfter ;
+      }
+      
+      
+
       if (valData1 > 255){
         valData1 = 255;
       }else if (valData1 < 0){
@@ -289,15 +321,16 @@ function showTransformForm() {
 
       if (valData2 > 255){
         valData2 = 255;
-      }else if (valData1 < 0){
+      }else if (valData2 < 0){
         valData2 = 0;
       }
-
       if (valData3 > 255){
         valData3 = 255;
-      }else if (valData1 < 0){
+      }else if (valData3 < 0){
         valData3 = 0;
-      }      
+      }
+
+     
       
       transformedImage.push(valData1, valData2, valData3, rgba[i + 3]);
     }
